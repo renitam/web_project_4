b1f67e01c35ea6259b38dc0c3cf4ac7d6c524ddf
xxengineer-practicum <85378780+xxengineer-practicum@users.noreply.github.com>
2021-10-03 06:27:29 -0400

    feat(validation): add validation styling and script

--- scripts/script.js
+++ scripts/script.js
@@ -36,6 +36,9 @@ const previewImg = modalPreview
   .querySelector(".modal__image");
 const previewCaption = modalPreview.querySelector(".modal__caption");
 
+//All Modals
+const allModals = document.querySelectorAll(".modal");
+
 //Card initialization values
 const initialCards = [
   {
@@ -66,6 +69,72 @@ const initialCards = [
 //
 
 // Functions
+
+// Add error message to modal input.
+function showInputError (modalEl, inputEl, errorMessage) {
+  const errorEl = modalEl.querySelector(`.modal__input-error_${inputEl.id}`);
+  inputEl.classList.add("modal__input_type_error");
+  errorEl.textContent = errorMessage;
+  errorEl.classList.add("modal__input-error_active");
+};
+
+// Remove error message from modal input.
+function hideInputError (modalEl, inputEl) {
+  const errorEl = modalEl.querySelector(`.modal__input-error_${inputEl.id}`);
+  inputEl.classList.remove("modal__input_type_error");
+  errorEl.classList.remove("modal__input-error_active");
+  errorEl.textContent = "";
+}
+
+//  Check input validity and toggle error message.
+function checkInputValidity (modalEl, inputEl) {
+  if (!inputEl.validity.valid) {
+    showInputError(modalEl, inputEl, inputEl.validationMessage);
+  } else {
+    hideInputError(modalEl, inputEl);
+  }
+}
+
+// Return true if inputs have invalid text. Used to toggle button state.
+function hasInvalidInput (inputList) {
+  return inputList.some((inputEl) => {
+    return !inputEl.validity.valid;
+  });
+}
+
+// Activate or deactivate button element based on input validity.
+function toggleButtonState (inputList, buttonEl) {
+  if (hasInvalidInput(inputList)) {
+    buttonEl.classList.add("modal__save_inactive");
+    buttonEl.setAttribute("disabled", "");
+  } else {
+    buttonEl.classList.remove("modal__save_inactive");
+    buttonEl.removeAttribute("disabled", "");
+  }
+}
+
+// Add error message events to inputs of modal.
+function setEventListeners (modalEl) {
+  const inputList = Array.from(modalEl.querySelectorAll(".modal__input"));
+  const buttonEl = modalEl.querySelector(".modal__save");
+  toggleButtonState(inputList,  buttonEl);
+
+  inputList.forEach( (inputEl) => {
+    inputEl.addEventListener("input", function () {
+      checkInputValidity(modalEl, inputEl);
+      toggleButtonState(inputList, buttonEl);
+    });
+  });
+}
+
+function enableValidation() {
+  const modalList = Array.from(document.querySelectorAll(".modal__form"));
+  modalList.forEach((modalEl) => {
+    modalList.forEach((modal) => {
+      setEventListeners(modal);
+    });
+  });
+}
 
 // Open modal window element
 function openModal(modal) {
@@ -186,6 +255,9 @@ function handleCardSubmit(evt) {
 
 // Scripts
 
+//Form Validation
+enableValidation();
+
 //Close modal
 closeModalBtns.forEach((button) => {
   button.addEventListener("click", initializeCloseBtns);
@@ -199,3 +271,4 @@ editProfileBtn.addEventListener("click", openProfileForm);
 initialCards.reverse().forEach((card) => addCard(createCard(card)));
 addCardBtn.addEventListener("click", openCardForm);
 formCardEl.addEventListener("submit", handleCardSubmit);
+
