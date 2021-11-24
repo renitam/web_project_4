7fc435ca2b82404ca0d9acd54b891328aad77a94
xxengineer-practicum <85378780+xxengineer-practicum@users.noreply.github.com>
2021-11-24 05:39:37 -0500

    hotfix(refactor): updates to code per reviewer notes

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,36 @@
 import * as utils from "./utils.js";
 import Card from "./Card.js";
+import FormValidator from "./FormValidator.js";
 
 
 // Variables
 
+const allModals = document.querySelectorAll(".modal");
+const modalCard = document.querySelector(".modal_type_card");
+const modalProfile = document.querySelector(".modal_type_profile");
+
+const closeModalBtns = document.querySelectorAll(".modal__close-btn");
+
 //Profile elements
-const editProfileBtn = utils.profile.querySelector(".profile__edit-btn");
-const addCardBtn = utils.profile.querySelector(".profile__add-btn");
-const formProfileEl = utils.modalProfile.querySelector(".modal__form_type_profile");
+const profile = document.querySelector(".profile");
+
+const editProfileBtn = profile.querySelector(".profile__edit-btn");
+const addCardBtn = profile.querySelector(".profile__add-btn");
+const formProfileEl = modalProfile.querySelector(".modal__form_type_profile");
+
+const nameInput = formProfileEl.querySelector("input[name='name']");
+const jobInput = formProfileEl.querySelector("input[name='career']");
+const nameField = profile.querySelector(".profile__name");
+const jobField = profile.querySelector(".profile__career");
+
 
 //Add Card Modal elements
-const formCardEl = utils.modalCard.querySelector(".modal__form_type_card");
+const formCardEl = modalCard.querySelector(".modal__form_type_card");
+const placeInput = modalCard.querySelector("input[name='place']");
+const linkInput = modalCard.querySelector("input[name='image']");
+
+
+
 
 //Card initialization values
 const initialCards = [
@@ -48,30 +68,84 @@ const initialCards = [
 // Open edit profile menu
 function openProfileForm() {
   // Initialize form values
-  utils.nameInput.value = utils.nameField.textContent;
-  utils.jobInput.value = utils.jobField.textContent;
+  nameInput.value = nameField.textContent;
+  jobInput.value = jobField.textContent;
 
   // Open modal
-  utils.openModal(utils.modalProfile);
+  utils.openModal(modalProfile);
 }
 
 // Open add card modal
 function openCardForm() {
   formCardEl.reset();
-  utils.openModal(utils.modalCard);
+  utils.openModal(modalCard);
 }
 
+
+// Submit new profile name and job title and close menu
+function handleProfileSubmit(evt) {
+  evt.preventDefault();
+
+  // Insert new values using the textContent property
+  nameField.textContent = nameInput.value;
+  jobField.textContent = jobInput.value;
+
+  // Close modal after saving
+  utils.closeModal(modalProfile);
+}
+
+function handleCardSubmit(evt) {
+  evt.preventDefault();
+  //Create an array of the placeInput & imageInput vars
+  const cardDetails = { name: placeInput.value, link: linkInput.value };
+
+  const newCard = new Card(cardDetails, "#card");
+  newCard.createCard();
+
+  utils.closeModal(modalCard);
+}
+
+// Make modal close button clickable
+closeModalBtns.forEach((button) => {
+  button.addEventListener("click", utils.useCloseBtn);
+});
+
+allModals.forEach( (modal) => {
+  modal.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("modal")) {
+      utils.closeModal(modal);
+    }
+  });
+
+  document.addEventListener("keydown", utils.escModal);
+});
+
+// Validation calls
+export const formValidationConfig = {
+  formSel: ".modal__form",
+  inputSel: ".modal__input",
+  submitBtnSel: ".modal__save",
+  inactiveBtnClass: "modal__save_inactive",
+  inputErrClass: "modal__input_type_error",
+  errorClass: "modal__input-error"
+}
+
+export const addFormValidator = new FormValidator(formValidationConfig, modalCard);
+addFormValidator.enableValidation();
+
+export const editFormValidator = new FormValidator(formValidationConfig, modalProfile);
+editFormValidator.enableValidation();
 
 // When user clicks Esc, close modal window.
 
 //Profile calls
-formProfileEl.addEventListener("submit", utils.handleProfileSubmit);
+formProfileEl.addEventListener("submit", handleProfileSubmit);
 editProfileBtn.addEventListener("click", openProfileForm);
 
 //Card calls
  //initialCards.reverse().forEach( (card) => createCard(card));
 addCardBtn.addEventListener("click", openCardForm);
-formCardEl.addEventListener("submit", utils.handleCardSubmit);
+formCardEl.addEventListener("submit", handleCardSubmit);
 
 
 
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,24 +1,5 @@
-import FormValidator from "./FormValidator.js";
-import Card from "./Card.js";
 
 // Constants
-
-export const allModals = document.querySelectorAll(".modal");
-export const modalProfile = document.querySelector(".modal_type_profile");
-export const modalCard = document.querySelector(".modal_type_card");
-
-export const closeModalBtns = document.querySelectorAll(".modal__close-btn");
-
-export const profile = document.querySelector(".profile");
-const formProfileEl = modalProfile.querySelector(".modal__form_type_profile");
-
-export const nameInput = formProfileEl.querySelector("input[name='name']");
-export const jobInput = formProfileEl.querySelector("input[name='career']");
-export const nameField = profile.querySelector(".profile__name");
-export const jobField = profile.querySelector(".profile__career");
-const placeInput = modalCard.querySelector("input[name='place']");
-const linkInput = modalCard.querySelector("input[name='image']");
-
 
 // Functions that open and close modals
 
@@ -27,13 +8,10 @@ export function openModal(modal) {
   document.addEventListener("keydown", escModal);
 }
 
+// Removed ternary operation since it's optional from project 6
 export function closeModal(modal) {
   modal.classList.remove("modal_display");
   document.removeEventListener("keydown", escModal);
-
-  modal.classList.contains("modal_type_card") ?
-    addFormValidator.resetValidation() :
-    editFormValidator.resetValidation();
 }
 
 export function escModal(evt) {
@@ -48,58 +26,3 @@ export function escModal(evt) {
 export function useCloseBtn(evt) {
   closeModal(evt.target.closest(".modal"));
 }
-
-
-// Submit new profile name and job title and close menu
-export function handleProfileSubmit(evt) {
-  evt.preventDefault();
-
-  // Insert new values using the textContent property
-  nameField.textContent = nameInput.value;
-  jobField.textContent = jobInput.value;
-
-  // Close modal after saving
-  closeModal(modalProfile);
-}
-
-export function handleCardSubmit(evt) {
-  evt.preventDefault();
-  //Create an array of the placeInput & imageInput vars
-  const cardDetails = { name: placeInput.value, link: linkInput.value };
-
-  const newCard = new Card(cardDetails, "#card");
-  newCard.createCard();
-
-  closeModal(modalCard);
-}
-
-// Make modal close button clickable
-closeModalBtns.forEach((button) => {
-  button.addEventListener("click", useCloseBtn);
-});
-
-allModals.forEach( (modal) => {
-  modal.addEventListener("click", (evt) => {
-    if (evt.target.classList.contains("modal")) {
-      closeModal(modal);
-    }
-  });
-
-  document.addEventListener("keydown", escModal);
-});
-
-// Validation calls
-export const formValidationConfig = {
-  formSel: ".modal__form",
-  inputSel: ".modal__input",
-  submitBtnSel: ".modal__save",
-  inactiveBtnClass: "modal__save_inactive",
-  inputErrClass: "modal__input_type_error",
-  errorClass: "modal__input-error"
-}
-
-export const addFormValidator = new FormValidator(formValidationConfig, modalCard);
-addFormValidator.enableValidation();
-
-export const editFormValidator = new FormValidator(formValidationConfig, modalProfile);
-editFormValidator.enableValidation();
